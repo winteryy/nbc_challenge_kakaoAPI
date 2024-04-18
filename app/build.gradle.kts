996435068be2a6aca102b681b5bd05e152e2b4c6
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-04-18 15:37:36 +0900

    chore: Set build config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,14 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
+val properties = Properties()
+properties.load(rootProject.file("local.properties").inputStream())
+
+val kakaoApiKey = properties.getProperty("KAKAO_API_KEY") ?: "None"
 
 android {
     namespace = "com.winteryy.nbcchallengekakaoapi"
@@ -15,6 +22,8 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField("String", "KAKAO_API_KEY", kakaoApiKey)
     }
 
     buildTypes {
@@ -35,6 +44,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
@@ -44,7 +54,18 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //ViewPager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+    //Coil
+    implementation("io.coil-kt:coil:2.6.0")
+
+    //Coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
